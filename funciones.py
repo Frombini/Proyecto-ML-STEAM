8c89a9eec3c4a1cdc8b4de17119eadcf1d679b7d
Francisco Rombini <frankrombini@gmail.com>
2023-11-24 15:29:37 -0300

    Commit de funciones

--- funciones.py
+++ funciones.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+'''1.def PlayTimeGenre( genero : str ): Debe devolver año con mas horas jugadas para dicho género.
+Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}'''
+
 def PlayTimeGenre(genero):
     # Leemos el archivo consulta1.csv para utilizar la funcion
     consulta1 = pd.read_csv('consulta1.csv')
@@ -16,7 +19,97 @@ def PlayTimeGenre(genero):
     # Encontrar el año con más horas jugadas
     max_year = grouped_df.idxmax()
 
+    # Convertir max_year a un tipo de datos nativo de Python
+    max_year = int(max_year)
+
     # Devolver el resultado como un diccionario
     result = {"Año de lanzamiento con más horas jugadas para {}:".format(genero): max_year}
 
     return result
+
+def UserForGenre(genero:str):
+    consulta2 = pd.read_csv('consulta2.csv')
+    # Filtrar el DataFrame por el género dado
+    genre_data = consulta2[consulta2['Genres'] == genero]
+
+    # Encontrar al usuario con más horas jugadas para ese género
+    top_user = genre_data.loc[genre_data['PlaytimeForever'].idxmax()]['UserId']
+
+    # Crear una lista de acumulación de horas jugadas por año
+    hours_by_year = genre_data.groupby('ReleaseDate')['PlaytimeForever'].sum().reset_index()
+    hours_by_year = hours_by_year.rename(columns={'ReleaseDate': 'Año', 'PlaytimeForever': 'Horas'})
+    # Convertir las horas a enteros (int) dividiendo por 60
+    hours_by_year['Horas'] = (hours_by_year['Horas'] / 60).astype(int)
+    hours_list = hours_by_year.to_dict(orient='records')
+
+    # Crear el diccionario de retorno
+    result = {
+        "Usuario con más horas jugadas para Género {}".format(genero): top_user,
+        "Horas jugadas": hours_list
+    }
+
+    return result
+
+def UsersRecommend(anio: int):
+    # Leemos el archivo consulta1.csv para utilizar la funcion
+    consulta3 = pd.read_csv('consulta3.csv')
+    if type(anio) != int:
+        return {"Debes colocar el año en entero, EJ:2015"}
+    if anio < consulta3['YearPosted'].min() or anio > consulta3['YearPosted'].max():
+        return {"Año no encontrado en el conjunto de datos"}
+    
+    # Filtrar por el año especificado y reseñas recomendadas y positivas/neutrales
+    filtered_reviews = consulta3[(consulta3['YearPosted'] == anio)]
+    
+    # Obtener el top 3 de juegos más recomendados
+    top3_games = filtered_reviews.nlargest(3, 'total_sentiment_analysis')[['ItemName', 'total_sentiment_analysis']]
+    
+    # Crear el formato de retorno
+    retorno = [{f"Puesto {i+1}": juego} for i, juego in enumerate(top3_games['ItemName'])]
+
+    return retorno
+
+
+def UsersWorstDeveloper(anio):
+    consulta4 = pd.read_csv('consulta4.csv')
+    if type(anio) != int:
+        return {"Debes colocar el año en entero, EJ:2015"}
+    if anio < consulta4['YearPosted'].min() or anio > consulta4['YearPosted'].max():
+        return {"Año no encontrado en el conjunto de datos"}
+    # Filtrar el DataFrame para el año dado y donde las recomendaciones son False y comentarios negativos
+    filtered_df = consulta4[consulta4['YearPosted'] == anio]
+
+    # Seleccionar las 3 desarrolladoras con más revisiones negativas
+    top_worst = filtered_df.groupby('Developer')['CountSentiment'].sum().nlargest(3).reset_index()
+
+    # Crear la lista de resultados en el formato deseado
+    result = [{"Puesto {}: {}".format(i + 1, row['Developer'])} for i, row in top_worst.iterrows()]
+
+    return result
+ 
+def sentiment_analysis(desarrolladora:str):
+    consulta5 = pd.read_csv('consulta5.csv')
+    if type(desarrolladora) != str:
+        return "Debes colocar un desarrollador de tipo str, EJ:'Valve'"
+    if len(desarrolladora) == 0:
+        return "Debes poner un desarrolador en tipo String"
+    # Filtramos el DataFrame para la empresa desarrolladora especificada
+    filtered_df = consulta5[consulta5['Developer'] == desarrolladora]
+
+    # Contamos la cantidad de registros para cada categoría de sentimiento
+    sentiment_counts = filtered_df['sentiment_analysis'].value_counts()
+
+    # Creamos el diccionario de retorno
+    result = {desarrolladora: f"[Negative = {sentiment_counts.get(0, 0)}, Neutral = {sentiment_counts.get(1, 0)}, Positive = {sentiment_counts.get(2, 0)}]"}
+    return result
+
+def recomendacion_juego(id_juego):
+    # Cargamos el csv de consulta6 para alimentar la funcion
+    modelo_df = pd.read_csv('consulta6.csv')
+    # Buscar el índice del juego con la ID dada
+    indice_juego = modelo_df[modelo_df["ItemId"] == id_juego].index[0]
+
+    # Obtener las recomendaciones para ese juego
+    recomendaciones = modelo_df.iloc[indice_juego]["RecomendacionesTop5"]
+
+    return recomendaciones
--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import json
 import numpy as np
 from fastapi.responses import JSONResponse
 from funciones import PlayTimeGenre
+from funciones import UserForGenre
+from funciones import UsersRecommend
+from funciones import UsersWorstDeveloper
+from funciones import sentiment_analysis
+from funciones import recomendacion_juego
 
 app = FastAPI()
 
 @app.get('/')
 def hola():
     return {'Esta es mi APi, Soy Francisco Rombini'}
-
-'''1.def PlayTimeGenre( genero : str ): Debe devolver año con mas horas jugadas para dicho género.
-Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}'''
 
 @app.get("/PlayTimeGenre/{genero}")
 async def user(genero: str):
@@ -21,3 +23,45 @@ async def user(genero: str):
         return resultado
     except Exception as e:
         return {"error": str(e)}   
+    
+@app.get("/UserForGenre/{genero}")
+async def user(genero: str):
+    try:
+        resultado = UserForGenre(genero)
+        return resultado
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get("/UsersRecommend/{anio}")
+async def user(anio:int):
+    try:
+        resultado = UsersRecommend(anio)
+        return resultado
+    except Exception as e:
+        return {"error": str(e)}
+    
+@app.get("/UsersWorstDeveloper/{anio}")
+async def user(anio:int):
+    try:
+        resultado = UsersWorstDeveloper(anio)
+        return resultado
+    except Exception as e:
+        return {"error": str(e)}  
+    
+
+@app.get("/sentiment_analysis/{desarrolladora}")
+async def user(desarrolladora:str):
+    try:
+        resultado = sentiment_analysis(desarrolladora)
+        return resultado
+    except Exception as e:
+        return {"error": str(e)}  
+    
+
+@app.get("/recomendacion_juego/{id_juego}")
+async def user(id_juego:int):
+    try:
+        resultado = recomendacion_juego(id_juego)
+        return resultado
+    except Exception as e:
+        return {"error": str(e)}     
