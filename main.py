606cfe90c71280cb039bdbaf90a8dc565608f3e5
Francisco Rombini <frankrombini@gmail.com>
2023-11-27 00:27:14 -0300

    Commit

--- main.py
+++ main.py
@@ -16,29 +16,54 @@ app = FastAPI()
 @app.get('/', response_class=HTMLResponse)
 def hola():
     html_content = """
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Endpoint-APIs de Francisco Rombini</title>
-    </head>
-    <body>
-        <h1>Bienvenidos a mi API</h1>
-        <p>Mi nombre es Francisco Rombini de la academia SOYHENRY, soy de la cohorte DataFT-17</p>
+        <!DOCTYPE html>
+        <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Endpoint-APIs de Francisco Rombini</title>
+                <style>
+                    body {
+                        font-family: 'Arial', sans-serif; /* Cambiar a la fuente deseada */
+                        background-color: #ffffff; /* Fondo blanco */
+                        color: #000000; /* Texto negro */
+                        margin: 20px;
+                    }
 
-        <h2>Endpoints Disponibles:</h2>
-        <ul>
-            <li><a href="https://proyecto-ml-steam.onrender.com/PlayTimeGenre/Action">/PlayTimeGenre/Action</a></li>
-            <li><a href="https://proyecto-ml-steam.onrender.com/UserForGenre/Adventure">/UserForGenre/Adventure</a></li>
-            <li><a href="https://proyecto-ml-steam.onrender.com/UsersRecommend/2012">/UsersRecommend/2012</a></li>
-            <li><a href="https://proyecto-ml-steam.onrender.com/UsersWorstDeveloper/2014">/UsersWorstDeveloper/2014</a></li>
-            <li><a href="https://proyecto-ml-steam.onrender.com/sentiment_analysis/Valve">/sentiment_analysis/Valve</a></li>
-            <li><a href="https://proyecto-ml-steam.onrender.com/recomendacion_juego/10">/recomendacion_juego/10</a></li>
-        </ul>
-    </body>
-</html>
-"""
+                    h1 {
+                        font-size: 2em; /* Tamaño de la fuente para h1 */
+                    }
+
+                    h2 {
+                        font-size: 1.5em; /* Tamaño de la fuente para h2 */
+                    }
+
+                    ul {
+                        list-style-type: none; /* Elimina los puntos de la lista */
+                        padding: 0;
+                    }
+
+                    li {
+                        margin-bottom: 10px; /* Espacio entre elementos de la lista */
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Bienvenidos a mi API</h1>
+                <p>Mi nombre es Francisco Rombini de la academia SOYHENRY, soy de la cohorte DataFT-17</p>
+
+                <h2>Endpoints Disponibles:</h2>
+                <ul>
+                    <li><strong>/PlayTimeGenre/Action:</strong> Devuelve el año con mas tiempo de juego para un género específico.</li>
+                    <li><strong>/UserForGenre/Adventure:</strong> Proporciona detalles sobre el usuario que acumula más horas jugadas para el género dado.</li>
+                    <li><strong>/UsersRecommend/2012:</strong> Devuelve el top 3 de juegos mas recomendados por usuarios para el año dado.</li>
+                    <li><strong>/UsersWorstDeveloper/2014:</strong> Devuelve el top 3 de desarrolladoras con juegos menos recomendados por usuarios para el año dado.</li>
+                    <li><strong>/sentiment_analysis/Valve:</strong> Devuelve análisis de sentimientos para un desarrollador específico.</li>
+                    <li><strong>/recomendacion_juego/10:</strong> Recomienda 5 juegos similares al id de juego ingresado.</li>
+                </ul>
+            </body>
+        </html>
+    """
     return HTMLResponse(content=html_content)
 
 
